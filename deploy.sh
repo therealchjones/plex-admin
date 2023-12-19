20770f4b4ba148d8d4cbb5030238af91b99cc1fe
Christian Jones <chjones@aleph0.com>
2023-12-19 12:00:30 -0500

    Add admin-secrets_local.sh
    for better continued management of a "secret" secrets file within the repo directory tree

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,17 @@ set -u
 set -e
 
 DEPLOY_SERVER=plex
-DEPLOY_WEB_PATH=www/chjones.server1359.seedhost.eu/chjones/admin/
-DEPLOY_CGI_PATH="$DEPLOY_WEB_PATH"/api
-DEPLOY_SECRETS_PATH=.config/plex-admin/
+DEPLOY_WEB_DIR=www/chjones.server1359.seedhost.eu/chjones/admin/
+DEPLOY_CGI_PATH="$DEPLOY_WEB_DIR"/api/index.cgi
+DEPLOY_SECRETS_PATH=.config/plex-admin/admin-secrets.sh
 
-scp index.html script.js style.css "$DEPLOY_SERVER":"$DEPLOY_WEB_PATH"
-scp admin-secrets.sh "$DEPLOY_SERVER":"$DEPLOY_SECRETS_PATH"
+DEPLOY_SECRETS_FILE=admin-secrets.sh
+DEPLOY_SECRETS_ALT_FILE=admin-secrets_local.sh
+
+scp index.html script.js style.css "$DEPLOY_SERVER":"$DEPLOY_WEB_DIR"
 scp api/index.cgi "$DEPLOY_SERVER":"$DEPLOY_CGI_PATH"
+
+if [ -r "$DEPLOY_SECRETS_ALT_FILE" ]; then
+	DEPLOY_SECRETS_FILE="$DEPLOY_SECRETS_ALT_FILE"
+fi
+scp "$DEPLOY_SECRETS_FILE" "$DEPLOY_SERVER":"$DEPLOY_SECRETS_PATH"
